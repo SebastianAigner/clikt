83739c685682b42dfcd900c4ac607b78de0b7d4e
AJ <aj@alt>
2022-08-20 09:29:39 -0700

    Pass localization to formatMessage

--- clikt/src/commonTest/kotlin/com/github/ajalt/clikt/testing/utils.kt
+++ clikt/src/commonTest/kotlin/com/github/ajalt/clikt/testing/utils.kt
@@ -1,5 +1,6 @@
 package com.github.ajalt.clikt.testing
 
 import com.github.ajalt.clikt.core.UsageError
+import com.github.ajalt.clikt.output.defaultLocalization
 
-val Throwable.formattedMessage: String? get() = (this as? UsageError)?.formatMessage() ?: message
+val Throwable.formattedMessage: String? get() = (this as? UsageError)?.formatMessage(defaultLocalization) ?: message
